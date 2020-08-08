ef2b5da0c08000255452a6a6947acb30783bd726
yingzhuo <yingzhor@gmail.com>
2020-08-09 05:25:18 +0800

    yingzhuo

---
+++ examples-password-encoder/src/main/java/carnival/examples/controller/PasswordEncoderController.java
@@ -0,0 +1,16 @@
+package carnival.examples.controller;
+
+import com.github.yingzhuo.carnival.password.PasswordEncoderUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class PasswordEncoderController {
+
+    @PostMapping("/encode-pwd")
+    public String encode(@RequestParam("password") String password) {
+        return PasswordEncoderUtils.encode(password);
+    }
+
+}
